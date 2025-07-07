0947e26aa0675a4d92707e7b0f82a7831cde1cd7
ALI <alinasserkhazali@gmail.com>
2025-07-07 12:02:38 +0300

    Initial Commit

---
+++ BlueTradingApp/BlueTradingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlueTradingAppApp.swift
+//  BlueTradingApp
+//
+//  Created by Ali Al-Khazali on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct BlueTradingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlueTradingApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BlueTradingApp
+//
+//  Created by Ali Al-Khazali on 7/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
